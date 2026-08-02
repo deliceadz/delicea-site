d3c60d6717a5d8f9dc523d590f45785cd68a8ea1
deliceadz <al.azhar20211@gmail.com>
2026-08-03 00:29:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-document.addEventListener("DOMContentLoaded", function () {
-console.log("Site Délicea chargé avec succès !");
+document.addEventListener("DOMContentLoaded", () => {
+
+const radios = document.querySelectorAll('input[name="pack"]');
+const bouton = document.getElementById("commander");
+
+const prix = {
+"essentiel": "Commandant • 1950 DA",
+"premium": "Commandant • 2550 DA",
+"chef": "Commandant • 2600 DA",
+"cuisine": "Commandant • 2800 DA"
+};
+
+radios.forEach(radio => {
+radio.addEventListener("change", () => {
+bouton.textContent = prix[radio.value];
 });
+});
+
+});
+
