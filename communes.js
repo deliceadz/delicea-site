2c27b66c362ff5bd6003d2a26665de6cb2f2b3a7
deliceadz <al.azhar20211@gmail.com>
2026-08-03 19:29:54 +0100

    Update communes.js

--- communes.js
+++ communes.js
@@ -1,78 +1,888 @@
+// ==========================
+// DELICEA - COMMUNES ALGERIE
+// PARTIE 1 : WILAYAS 01 - 10
+// ==========================
+
+
 const communes = {
 
-"Alger":[
-"Alger Centre",
-"Bab El Oued",
-"El Harrach",
-"Hussein Dey",
-"Bir Mourad Raïs",
-"Dar El Beida"
+
+"1 - Adrar":[
+"Adrar",
+"Tamest",
+"Charouine",
+"Reggane",
+"In Zghmir",
+"Tit",
+"Akabli",
+"Tsabit",
+"Zaouiet Kounta",
+"Aoulef",
+"Timokten",
+"Tamentit",
+"Fenoughil",
+"Ouled Ahmed Timmi",
+"Bordj Badji Mokhtar"
 ],
 
-"Blida":[
+
+
+"2 - Chlef":[
+"Chlef",
+"Ténès",
+"Beni Haoua",
+"El Karimia",
+"Talassa",
+"Ouled Fares",
+"Sendjas",
+"Oued Fodda",
+"Zeboudja",
+"Boukadir",
+"El Marsa",
+"Breira",
+"Ouled Ben Abdelkader",
+"Chettia",
+"Medjadja"
+],
+
+
+
+
+"3 - Laghouat":[
+"Laghouat",
+"Ksar El Hirane",
+"Aflou",
+"Brida",
+"El Assafia",
+"Gueltat Sidi Saad",
+"Ain Madhi",
+"Taouiala",
+"Sebgag",
+"Hassi R'Mel",
+"El Haouaita",
+"Oued Morra",
+"Tadjmout"
+],
+
+
+
+
+"4 - Oum El Bouaghi":[
+"Oum El Bouaghi",
+"Ain Beida",
+"Ain M'lila",
+"Ain Fakroun",
+"Sigus",
+"Souk Naamane",
+"Fkirina",
+"El Amiria",
+"Behir Chergui",
+"El Djazia",
+"Ouled Gacem",
+"Berriche"
+],
+
+
+
+
+"5 - Batna":[
+"Batna",
+"Barika",
+"Merouana",
+"Arris",
+"N'Gaous",
+"Tazoult",
+"Seggana",
+"Chemora",
+"Seriana",
+"Ain Touta",
+"El Madher",
+"Ras El Aioun",
+"Ouled Si Slimane",
+"Fesdis"
+],
+
+
+
+
+"6 - Béjaïa":[
+"Béjaïa",
+"Amizour",
+"Akbou",
+"Kherrata",
+"El Kseur",
+"Souk El Tenine",
+"Tichy",
+"Timezrit",
+"Seddouk",
+"Chemini",
+"Darguina",
+"Barbacha",
+"Ighil Ali"
+],
+
+
+
+
+"7 - Biskra":[
+"Biskra",
+"Ouled Djellal",
+"Sidi Okba",
+"Zeribet El Oued",
+"Tolga",
+"El Kantara",
+"Foughala",
+"Doucen",
+"Lichana",
+"M'Chouneche",
+"Branis"
+],
+
+
+
+
+"8 - Béchar":[
+"Béchar",
+"Abadla",
+"Kenadsa",
+"Taghit",
+"Beni Ounif",
+"Lahmar",
+"Erg Ferradj",
+"Meridja"
+],
+
+
+
+
+"9 - Blida":[
 "Blida",
 "Boufarik",
 "El Affroun",
-"Mouzaïa",
-"Ouled Yaïch"
+"Mouzaia",
+"Ouled Yaich",
+"Larbaa",
+"Meftah",
+"Chebli",
+"Beni Tamou",
+"Chiffa",
+"Soumaa",
+"Chrea"
 ],
 
-"Tipaza":[
-"Tipaza",
-"Koléa",
-"Fouka",
-"Douaouda",
-"Hadjout"
+
+
+
+"10 - Bouira":[
+"Bouira",
+"Sour El Ghozlane",
+"M'Chedallah",
+"Lakhdaria",
+"Bechloul",
+"El Esnam",
+"Haizer",
+"Ain Bessem",
+"Bir Ghbalou",
+"Kadiria",
+"Chorfa",
+"Taghzout"
 ],
 
-"Oran":[
+
+  
+"11 - Tamanrasset":[
+"Tamanrasset",
+"Abalessa",
+"In Amguel",
+"Tazrouk",
+"Idles",
+"Tin Zaouatine"
+],
+
+
+
+"12 - Tébessa":[
+"Tébessa",
+"Bir El Ater",
+"El Aouinet",
+"El Kouif",
+"El Ogla",
+"Negrine",
+"Cheria",
+"Bekaria",
+"Morsott",
+"Bir Mokkadem",
+"Saf Saf El Ouesra"
+],
+
+
+
+"13 - Tlemcen":[
+"Tlemcen",
+"Maghnia",
+"Remchi",
+"Sebdou",
+"Ghazaouet",
+"Nedroma",
+"Hennaya",
+"Mansourah",
+"Ouled Mimoun",
+"Bab El Assa",
+"Fellaoucene",
+"Beni Snous"
+],
+
+
+
+"14 - Tiaret":[
+"Tiaret",
+"Sougueur",
+"Frenda",
+"Mahdia",
+"Ksar Chellala",
+"Mechraa Safa",
+"Ain Deheb",
+"Rahouia",
+"Medroussa",
+"Tousnina",
+"Dahmouni"
+],
+
+
+
+"15 - Tizi Ouzou":[
+"Tizi Ouzou",
+"Azazga",
+"Larbaa Nath Irathen",
+"Ouaguenoun",
+"Makouda",
+"Freha",
+"Boghni",
+"Drâa El Mizan",
+"Ouadhias",
+"Ain El Hammam",
+"Mizrana",
+"Tigzirt"
+],
+
+
+
+"16 - Alger":[
+"Alger Centre",
+"Sidi M'Hamed",
+"El Madania",
+"Belouizdad",
+"Bab El Oued",
+"Casbah",
+"Hussein Dey",
+"El Harrach",
+"Baraki",
+"Dar El Beida",
+"Rouiba",
+"Zéralda",
+"Staoueli",
+"Chéraga",
+"Draria",
+"Birkhadem",
+"Bir Mourad Rais"
+],
+
+
+
+"17 - Djelfa":[
+"Djelfa",
+"Messaad",
+"Ain Oussera",
+"Hassi Bahbah",
+"El Idrissia",
+"Dar Chioukh",
+"Charef",
+"Faidh El Botma",
+"Zaafrane",
+"Selmana"
+],
+
+
+
+"18 - Jijel":[
+"Jijel",
+"El Milia",
+"Taher",
+"Ziama Mansouriah",
+"Chekfa",
+"Texenna",
+"Kaous",
+"Settara",
+"El Aouana",
+"Ouled Yahia Khedrouche"
+],
+
+
+
+"19 - Sétif":[
+"Sétif",
+"El Eulma",
+"Ain Arnat",
+"Ain Oulmene",
+"Bougaa",
+"Amoucha",
+"Beni Ourtilane",
+"Djemila",
+"Guidjel",
+"Bir El Arch",
+"Ras El Oued"
+],
+
+
+
+"20 - Saïda":[
+"Saïda",
+"Ouled Brahim",
+"Ain El Hadjar",
+"El Hassasna",
+"Sidi Boubekeur",
+"Daoud",
+"Maamora"
+],
+
+  
+
+"21 - Skikda":[
+"Skikda",
+"El Harrouch",
+"Collo",
+"Azzaba",
+"Ramdane Djamel",
+"Tamalous",
+"Ben Azzouz",
+"Ouled Attia",
+"Zitouna",
+"Ain Kechra",
+"Es Sebt"
+],
+
+
+
+"22 - Sidi Bel Abbès":[
+"Sidi Bel Abbès",
+"Telagh",
+"Ben Badis",
+"Sfisef",
+"Marhoum",
+"Ras El Ma",
+"Mostefa Ben Brahim",
+"Moulay Slissen",
+"Merine",
+"Tabia"
+],
+
+
+
+"23 - Annaba":[
+"Annaba",
+"El Bouni",
+"El Hadjar",
+"Berrahal",
+"Chetaibi",
+"Seraidi",
+"Treat",
+"Oued El Aneb"
+],
+
+
+
+"24 - Guelma":[
+"Guelma",
+"El Fedjoudj",
+"Héliopolis",
+"Bouchegouf",
+"Oued Zenati",
+"Guelaat Bousbaa",
+"Ain Makhlouf",
+"Khezara",
+"Belkheir",
+"Bordj Sabat"
+],
+
+
+
+"25 - Constantine":[
+"Constantine",
+"El Khroub",
+"Ain Smara",
+"Hamma Bouziane",
+"Didouche Mourad",
+"Zighoud Youcef",
+"Ibn Ziad",
+"Ouled Rahmoune"
+],
+
+
+
+"26 - Médéa":[
+"Médéa",
+"Berrouaghia",
+"Ksar El Boukhari",
+"Tablat",
+"El Omaria",
+"Beni Slimane",
+"Ouamri",
+"Chahbounia",
+"Si Mahdjoub",
+"Aziz"
+],
+
+
+
+"27 - Mostaganem":[
+"Mostaganem",
+"Fornaka",
+"Ain Nouissy",
+"Mesra",
+"Sour",
+"Hassi Mamèche",
+"Mazagran",
+"Kheir Eddine",
+"Sidi Ali",
+"Achaacha"
+],
+
+
+
+"28 - M'Sila":[
+"M'Sila",
+"Bou Saada",
+"Sidi Aissa",
+"Maadid",
+"Magra",
+"Ouled Derradj",
+"Berhoum",
+"Ain El Melh",
+"Belaiba",
+"Hammam Dhalaa"
+],
+
+
+
+"29 - Mascara":[
+"Mascara",
+"Tighennif",
+"Ghriss",
+"Sig",
+"Froha",
+"Oued Taria",
+"Zahana",
+"El Bordj",
+"Ain Fares",
+"Hacine"
+],
+
+
+
+"30 - Ouargla":[
+"Ouargla",
+"Rouissat",
+"Ngoussa",
+"Hassi Messaoud",
+"Taibet",
+"Sidi Khouiled",
+"Ain Beida",
+"El Hadjira",
+"Meggarine"
+],
+
+  
+
+"31 - Oran":[
 "Oran",
 "Bir El Djir",
 "Es Senia",
 "Arzew",
-"Aïn El Turk"
+"Bethioua",
+"Gdyel",
+"El Kerma",
+"Mers El Hadjadj",
+"Ain El Turk",
+"Fleurus",
+"Kristel",
+"Hassi Bounif"
 ],
 
-"Constantine":[
-"Constantine",
-"El Khroub",
-"Aïn Smara",
-"Hamma Bouziane"
+
+
+"32 - El Bayadh":[
+"El Bayadh",
+"Rogassa",
+"Brezina",
+"El Abiodh Sidi Cheikh",
+"Boualem",
+"Cheguig",
+"Arbaouat",
+"Tousmouline",
+"Kef Lahmar"
 ],
 
-"Sétif":[
-"Sétif",
-"El Eulma",
-"Aïn Arnat",
-"Aïn Oulmene"
+
+
+"33 - Illizi":[
+"Illizi",
+"Djanet",
+"Debdeb",
+"In Amenas",
+"Bordj Omar Driss"
+],
+
+
+
+"34 - Bordj Bou Arréridj":[
+"Bordj Bou Arréridj",
+"Ras El Oued",
+"Bordj Ghdir",
+"El Achir",
+"Bir Kasd Ali",
+"Medjana",
+"Djaafra",
+"Hasnaoua",
+"Teniet En Nasr",
+"Mansoura"
+],
+
+
+
+"35 - Boumerdès":[
+"Boumerdès",
+"Dellys",
+"Thenia",
+"Boudouaou",
+"Isser",
+"Baghlia",
+"Naciria",
+"Khemis El Khechna",
+"Corso",
+"Zemmouri",
+"Timezrit"
+],
+
+
+
+"36 - El Tarf":[
+"El Tarf",
+"El Kala",
+"Dréan",
+"Ben M'Hidi",
+"Besbes",
+"Echatt",
+"Zerizer",
+"Chefia",
+"Asfour"
+],
+
+
+
+"37 - Tindouf":[
+"Tindouf",
+"Oum El Assel"
+],
+
+
+
+"38 - Tissemsilt":[
+"Tissemsilt",
+"Theniet El Had",
+"Lardjem",
+"Bordj Bounaama",
+"Ouled Bessem",
+"Khemisti",
+"Layoune",
+"Ammari",
+"Maacem"
+],
+
+
+
+"39 - El Oued":[
+"El Oued",
+"Robbah",
+"Guemar",
+"Reguiba",
+"Debila",
+"Magrane",
+"Bayadha",
+"Trifaoui",
+"Hassani Abdelkrim",
+"Nakhla"
+],
+
+
+
+"40 - Khenchela":[
+"Khenchela",
+"Kaïs",
+"Chechar",
+"Babar",
+"El Hamma",
+"Ouled Rechache",
+"Yabous",
+"Baghai",
+"Tamza"
+],
+
+
+
+"41 - Souk Ahras":[
+"Souk Ahras",
+"Sedarata",
+"M'daourouch",
+"Taoura",
+"Oued Keberit",
+"Mechroha",
+"Ouled Driss",
+"Merahna",
+"Hanencha"
+],
+
+
+
+"42 - Tipaza":[
+"Tipaza",
+"Koléa",
+"Cherchell",
+"Fouka",
+"Bou Ismail",
+"Hadjout",
+"Attatba",
+"Ahmer El Ain",
+"Gouraya",
+"Sidi Amar",
+"Menaceur",
+"Messelmoun"
+],
+
+
+
+"43 - Mila":[
+"Mila",
+"Ferdjioua",
+"Grarem Gouga",
+"Teleghma",
+"Oued Athmania",
+"Rouached",
+"Tadjenanet",
+"Terrai Bainen",
+"Sidi Merouane",
+"Ain Beida Harriche"
+],
+
+
+
+"44 - Aïn Defla":[
+"Aïn Defla",
+"Khemis Miliana",
+"Miliana",
+"El Attaf",
+"Bordj Emir Khaled",
+"Arib",
+"Rouina",
+"El Amra",
+"Bathia",
+"Djelida",
+"Mekhatria"
+],
+
+
+
+"45 - Naâma":[
+"Naâma",
+"Mécheria",
+"Ain Sefra",
+"Tiout",
+"Moghrar",
+"Asla",
+"Djenien Bourezg",
+"Kasdir"
+],
+
+
+
+"46 - Aïn Témouchent":[
+"Aïn Témouchent",
+"Beni Saf",
+"El Malah",
+"Terga",
+"El Amria",
+"Hammam Bou Hadjar",
+"Chaabat El Ham",
+"Oulhaça Gheraba",
+"Tamzoura"
+],
+
+
+
+"47 - Ghardaïa":[
+"Ghardaïa",
+"El Meniaa",
+"Berriane",
+"Metlili",
+"Zelfana",
+"Guerrara",
+"Sebseb",
+"Dhayet Bendhahoua",
+"Mansoura"
+],
+
+
+
+"48 - Relizane":[
+"Relizane",
+"Oued Rhiou",
+"Yellel",
+"Ammi Moussa",
+"Mazouna",
+"Zemmora",
+"El Matmar",
+"Jdiouia",
+"Ain Tarek",
+"Kalaâ"
+],
+
+
+
+"49 - Timimoun":[
+"Timimoun",
+"Ouled Saïd",
+"Charouine",
+"Ouled Aïssa",
+"Tinerkouk",
+"Metarfa"
+],
+
+
+
+"50 - Bordj Badji Mokhtar":[
+"Bordj Badji Mokhtar",
+"Timiaouine"
+],
+ 
+
+
+"51 - Ouled Djellal":[
+"Ouled Djellal",
+"Besbes",
+"Ras El Miad",
+"Doucen",
+"Chaiba"
+],
+
+
+
+"52 - Béni Abbès":[
+"Béni Abbès",
+"Kerzaz",
+"Ouled Khoudir",
+"Tamtert",
+"Igli"
+],
+
+
+
+"53 - In Salah":[
+"In Salah",
+"Foggaret Ezzaouia",
+"Amguid"
+],
+
+
+
+"54 - In Guezzam":[
+"In Guezzam"
+],
+
+
+
+"55 - Touggourt":[
+"Touggourt",
+"Nezla",
+"Tebesbest",
+"Temacine",
+"Megarine",
+"El Alia",
+"Blidet Amor",
+"Taibet"
+],
+
+
+
+"56 - Djanet":[
+"Djanet",
+"Zaouiat Kounta"
+],
+
+
+
+"57 - El M'Ghair":[
+"El M'Ghair",
+"Djamaa",
+"Still",
+"Sidi Amrane",
+"Oum Touyour"
+],
+
+
+
+"58 - El Meniaa":[
+"El Meniaa",
+"Hassi Gara",
+"Hassi Fehal"
 ]
+
 
 };
 
 
 function chargerCommunes(){
 
-let wilaya=document.getElementById("wilaya").value;
 
-let select=document.getElementById("commune");
+let wilaya = document.getElementById("wilaya").value;
 
 
-select.innerHTML="<option>Choisir Commune</option>";
+let select = document.getElementById("commune");
+
+
+
+select.innerHTML = "<option>Choisir Commune</option>";
+
 
 
 if(communes[wilaya]){
 
-communes[wilaya].forEach(function(c){
 
-let option=document.createElement("option");
+    communes[wilaya].forEach(function(c){
 
-option.text=c;
-option.value=c;
 
-select.add(option);
+        let option = document.createElement("option");
 
-});
+
+        option.text = c;
+
+        option.value = c;
+
+
+        select.add(option);
+
+
+    });
+
 
 }
 
+
 }
+
+
+
+
+// Activation quand on change la wilaya
+
+document
+.getElementById("wilaya")
+.addEventListener("change", chargerCommunes);
