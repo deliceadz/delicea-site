b18c12187dbb6f60a2d31e2a41ccb17acbc039d4
deliceadz <al.azhar20211@gmail.com>
2026-08-03 02:11:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let packChoisi = "";
+let tarifLivraison = 0;
+
 
 function choisirPack(pack){
 
@@ -14,31 +16,91 @@ function choisirPack(pack){
 }
 
 
+
+function calculerLivraison(){
+
+    let wilaya = document.getElementById("wilaya").value;
+    let type = document.getElementById("typeLivraison").value;
+
+    let info = document.getElementById("infoLivraison");
+
+
+    if(livraisons[wilaya]){
+
+        if(type === "domicile"){
+            tarifLivraison = livraisons[wilaya].domicile;
+        }
+
+        else if(type === "bureau"){
+            tarifLivraison = livraisons[wilaya].bureau;
+        }
+
+        else{
+            tarifLivraison = 0;
+        }
+
+
+        info.innerHTML =
+        "🚚 Délai : " + livraisons[wilaya].delai +
+        "<br>Tarif livraison : " + tarifLivraison + " DA";
+
+
+    }else{
+
+        info.innerHTML="Choisir une wilaya";
+
+    }
+
+}
+
+
+
+
 function envoyerCommande(){
 
-    let nom = document.getElementById("nom").value;
-    let tel = document.getElementById("tel").value;
-    let wilaya = document.getElementById("wilaya").value;
-    let commune = document.getElementById("commune").value;
-    let adresse = document.getElementById("adresse").value;
+
+let nom = document.getElementById("nom").value;
+let tel = document.getElementById("tel").value;
+let wilaya = document.getElementById("wilaya").value;
+let type = document.getElementById("typeLivraison").value;
+let commune = document.getElementById("commune").value;
+let adresse = document.getElementById("adresse").value;
 
 
-    let message =
-    "Nouvelle commande Delicea%0A%0A" +
-    "Pack : " + packChoisi + "%0A" +
-    "Nom : " + nom + "%0A" +
-    "Téléphone : " + tel + "%0A" +
-    "Wilaya : " + wilaya + "%0A" +
-    "Commune : " + commune + "%0A" +
-    "Adresse : " + adresse;
+
+let message =
+
+"Nouvelle commande Delicea%0A%0A"+
+
+"📦 Pack : "+packChoisi+"%0A"+
+
+"👤 Nom : "+nom+"%0A"+
+
+"📞 Téléphone : "+tel+"%0A"+
+
+"📍 Wilaya : "+wilaya+"%0A"+
+
+"🚚 Livraison : "+type+"%0A"+
+
+"💰 Tarif livraison : "+tarifLivraison+" DA%0A"+
+
+"🏠 Commune : "+commune+"%0A"+
+
+"Adresse : "+adresse;
 
 
-    let numero = "213770715514";
+
+let numero="213770715514";
 
 
-    window.open(
-        "https://wa.me/" + numero + "?text=" + message,
-        "_blank"
-    );
+
+window.open(
+
+"https://wa.me/"+numero+"?text="+message,
+
+"_blank"
+
+);
+
 
 }
