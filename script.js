4e0cba9dee176dee1b9f94a1473ee62c3e64503a
deliceadz <al.azhar20211@gmail.com>
2026-08-03 01:18:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -157,4 +157,105 @@ document
 
     );
 
+});// ===============================
+// WILAYAS ALGERIE
+// Partie 2
+// ===============================
+
+const wilayas = [
+
+"Adrar",
+"Chlef",
+"Laghouat",
+"Oum El Bouaghi",
+"Batna",
+"Béjaïa",
+"Biskra",
+"Béchar",
+"Blida",
+"Bouira",
+"Tamanrasset",
+"Tébessa",
+"Tlemcen",
+"Tiaret",
+"Tizi Ouzou",
+"Alger",
+"Djelfa",
+"Jijel",
+"Sétif",
+"Saïda",
+"Skikda",
+"Sidi Bel Abbès",
+"Annaba",
+"Guelma",
+"Constantine",
+"Médéa",
+"Mostaganem",
+"M'Sila",
+"Mascara",
+"Ouargla",
+"Oran",
+"El Bayadh",
+"Illizi",
+"Bordj Bou Arreridj",
+"Boumerdès",
+"El Tarf",
+"Tindouf",
+"Tissemsilt",
+"El Oued",
+"Khenchela",
+"Souk Ahras",
+"Tipaza",
+"Mila",
+"Aïn Defla",
+"Naâma",
+"Aïn Témouchent",
+"Ghardaïa",
+"Relizane",
+"Timimoun",
+"Bordj Badji Mokhtar",
+"Ouled Djellal",
+"Béni Abbès",
+"In Salah",
+"In Guezzam",
+"Touggourt",
+"Djanet",
+"El M'Ghair",
+"El Meniaa"
+
+];
+
+const wilayaSelect = document.getElementById("wilaya");
+
+if (wilayaSelect) {
+
+wilayas.forEach((nom,index)=>{
+
+const option=document.createElement("option");
+
+option.value=nom;
+
+option.textContent=(index+1)+" - "+nom;
+
+wilayaSelect.appendChild(option);
+
 });
+
+}
+const communes = {
+  "Alger": [
+    "Sidi M'Hamed",
+    "El Madania",
+    "Belouizdad",
+    "Bab El Oued",
+    "Kouba",
+    "Hussein Dey"
+    // ...
+  ],
+  "Oran": [
+    "Oran",
+    "Bir El Djir",
+    "Es Senia"
+    // ...
+  ]
+};
