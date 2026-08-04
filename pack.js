7f5d88ff91067ca45a8519197ce0c03649065399
deliceadz <al.azhar20211@gmail.com>
2026-08-04 01:12:33 +0100

    Update pack.js

--- pack.js
+++ pack.js
@@ -1,152 +1,143 @@
 const packs = [
 
 {
-id:"kitchen",
-name:"باقة الكوزينة",
-price:2800,
-image:"pack1.jpg",
-weight:"وزن كل علبة 120غ",
-details:[
-"قرفة مرحية",
-"ثوم مرحي",
-"قصبر مرحي",
-"كمون مرحي",
-"بسباس حب",
-"أعشاب عطرية",
-"بابريكا مدخن",
-"فلفل أحمر حار مرحي",
-"زعفران مرحي",
-"رأس الحانوت",
-"توابل الدجاج",
-"توابل الكاري"
-]
+    id: "basic",
+    name: "الباقة الأساسية",
+    price: 1950,
+    image: "images/pack4.jpg",
+    weight: "وزن كل علبة 120غ",
+    details: [
+        "كركم مرحي",
+        "كمون مرحي",
+        "فلفل أسود مرحي",
+        "كروية مرحية",
+        "قصبر مرحي",
+        "فلفل عكري مرحي",
+        "فلفل حار مرحي",
+        "رأس الحانوت"
+    ]
 },
 
-
-
 {
-id:"mamira",
-name:"باقة المميرة",
-price:2550,
-image:"pack2.jpg",
-weight:"وزن كل علبة 120غ",
-details:[
-"فلفل عكري مرحي",
-"فلفل حار مرحي",
-"رأس الحانوت",
-"فلفل أسود مرحي",
-"قرفة مرحية",
-"أعشاب عطرية",
-"قرنفل حب",
-"زعفران مرحي",
-"بابريكا مدخن",
-"زعيترة"
-]
+    id: "kitchen",
+    name: "باقة الكوزينة",
+    price: 2800,
+    image: "images/pack1.jpg",
+    weight: "وزن كل علبة 120غ",
+    details: [
+        "قرفة مرحية",
+        "ثوم مرحي",
+        "قصبر مرحي",
+        "كمون مرحي",
+        "بسباس حب",
+        "أعشاب عطرية",
+        "بابريكا مدخن",
+        "فلفل أحمر حار مرحي",
+        "زعفران مرحي",
+        "رأس الحانوت",
+        "توابل الدجاج",
+        "توابل الكاري"
+    ]
 },
 
-
-
-
 {
-id:"chef",
-name:"باقة الشيف",
-price:2600,
-image:"pack3.jpg",
-weight:"وزن كل علبة 120غ",
-details:[
-"فلفل أسود مرحي",
-"إكليل الجبل",
-"فلفل عكري مرحي",
-"فلفل أحمر حار مرحي",
-"معدنوس مجفف",
-"بابريكا مدخن",
-"أعشاب عطرية",
-"سكنجبير مرحي",
-"قصبر مرحي",
-"ثوم مرحي"
-]
+    id: "mamira",
+    name: "باقة المميرة",
+    price: 2550,
+    image: "images/pack2.jpg",
+    weight: "وزن كل علبة 120غ",
+    details: [
+        "فلفل عكري مرحي",
+        "فلفل حار مرحي",
+        "رأس الحانوت",
+        "فلفل أسود مرحي",
+        "قرفة مرحية",
+        "أعشاب عطرية",
+        "قرنفل حب",
+        "زعفران مرحي",
+        "بابريكا مدخن",
+        "زعيترة"
+    ]
 },
 
-
-
-
 {
-id:"basic",
-name:"الباقة الأساسية",
-price:1950,
-image:"pack4.jpg",
-weight:"وزن كل علبة 120غ",
-details:[
-"كركم مرحي",
-"كمون مرحي",
-"فلفل أسود مرحي",
-"كروية مرحية",
-"قصبر مرحي",
-"فلفل عكري مرحي",
-"فلفل حار مرحي",
-"رأس الحانوت"
-]
-
+    id: "chef",
+    name: "باقة الشيف",
+    price: 2600,
+    image: "images/pack3.jpg",
+    weight: "وزن كل علبة 120غ",
+    details: [
+        "فلفل أسود مرحي",
+        "إكليل الجبل",
+        "فلفل عكري مرحي",
+        "فلفل أحمر حار مرحي",
+        "معدنوس مجفف",
+        "بابريكا مدخن",
+        "أعشاب عطرية",
+        "سكنجبير مرحي",
+        "قصبر مرحي",
+        "ثوم مرحي"
+    ]
 }
 
 ];
 
-
-
-
-
 const container = document.getElementById("packs-container");
 
-
 packs.forEach(pack => {
-
 
 container.innerHTML += `
 
 <div class="pack-card">
 
-
 <img src="${pack.image}" alt="${pack.name}">
-
 
 <h3>${pack.name}</h3>
 
-
 <p>${pack.weight}</p>
 
+<div class="price">${pack.price} دج</div>
 
-<h4>${pack.price} دج</h4>
+<div class="buttons">
 
-
-
-<button onclick="orderPack('${pack.name}',${pack.price})">
+<button class="order-btn"
+onclick="window.location.href='order.html?pack=${pack.id}'">
 اطلب الآن
 </button>
 
-
-
-<button onclick="showDetails('${pack.id}')">
+<button class="details-btn"
+onclick="toggleDetails('${pack.id}')">
 عرض التفاصيل
 </button>
 
+</div>
 
+<div class="details" id="${pack.id}">
 
-<div id="${pack.id}" class="details">
-
-<h4>مكونات الباقة:</h4>
+<h4>مكونات الباقة</h4>
 
 <ul>
 
-${pack.details.map(item=>`<li>${item}</li>`).join("")}
+${pack.details.map(item => `<li>${item}</li>`).join("")}
 
 </ul>
 
-
 </div>
-
 
 </div>
 
 `;
 
 });
+
+function toggleDetails(id){
+
+const box = document.getElementById(id);
+
+if(box.style.display === "block"){
+    box.style.display = "none";
+}else{
+    box.style.display = "block";
+}
+
+}
